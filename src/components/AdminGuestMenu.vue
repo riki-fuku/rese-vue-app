4cd71ada414b03ec0787a19cea782a03b681476b
FukushimaRiki <chsgrncc.0209@ezweb.ne.jp>
2024-03-27 22:04:07 +0900

    [一般ユーザー]基本画面作成完了

---
+++ src/components/AdminGuestMenu.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-app-bar app color="white">
+        <v-app-bar-nav-icon @click="showMenu = !showMenu">
+            <v-icon class="bg-blue-accent-3 pa-4" style="border-radius: 3px;">
+                {{ showMenu ? 'mdi-close' : 'mdi-menu' }}
+            </v-icon>
+        </v-app-bar-nav-icon>
+        <v-toolbar-title>Rese</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="showMenu" absolute temporary style="width: 100vw;">
+        <v-list-item to="/">
+            <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/">
+            <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/mypage">
+            <v-list-item-title>Mypage</v-list-item-title>
+        </v-list-item>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        showMenu: false
+    })
+}
+</script>
+
+<style scoped>
+.v-app-bar-nav-icon {
+    color: white;
+}
+
+.v-toolbar-title {
+    color: #3b82f6;
+}
+
+.v-navigation-drawer {
+    padding-top: 300px;
+}
+
+.v-list-item {
+    margin: 30px auto;
+    width: 200px;
+}
+
+.v-list-item-title {
+    text-align: center;
+    color: #3b82f6;
+}
+</style>
