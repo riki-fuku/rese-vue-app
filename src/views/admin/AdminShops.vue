19d0cde9efa613055c203d2dedf2abcce758105e
FukushimaRiki <chsgrncc.0209@ezweb.ne.jp>
2024-04-24 01:37:52 +0900

    [管理者画面]店舗CSVアップロード機能追加

--- src/views/admin/AdminShops.vue
+++ src/views/admin/AdminShops.vue
@@ -7,15 +7,20 @@
 
             <p class="pa-5 text-h6 font-weight-bold">新規店舗登録(CSVアップロード)</p>
 
-            <v-sheet class="pl-5 transparent-input">
+            <v-sheet class="pl-5 transparent-input ml-10 mb-10 mt-2">
+
+                <v-row class="mb-2">
+                    <p class="text-red" v-if="errorMessage">
+                        {{ errorMessage }}
+                    </p>
+                </v-row>
+
+
                 <v-row>
-                    <v-col cols="12" md="6">
-                        <v-file-input v-model="file" accept=".csv" label="CSVファイルを選択" outlined
-                            @change="handleFileUpload"></v-file-input>
-                    </v-col>
-                    <!-- <v-col cols="12" md="6">
-                        <v-btn class="mx-3 bg-deep-orange-darken-1" @click="submitCsv">新規登録</v-btn>
-                    </v-col> -->
+                    <input type="file" accept="text/*" @change="handleFileChange">
+                    <v-btn class="my-1 mx-3 bg-blue-accent-4" @click="storeShops()">
+                        CSVアップロード
+                    </v-btn>
                 </v-row>
             </v-sheet>
 
@@ -56,20 +61,74 @@ import { ref, onMounted } from 'vue'
 import AdminMenu from '@/components/AdminMenu.vue'
 import { useShopStore } from '@/stores/shops'
 import { useRouter } from 'vue-router'
+import axios from 'axios'
 
 const router = useRouter()
-
 const shopStore = useShopStore()
 
 const loading = ref(false) // ローディングフラグ
-
 const shops = ref(null) // 店舗代表者一覧
+const selectedFile = ref(null);
+const errorMessage = ref('') // エラーメッセージ
 
-const file = ref(null) // CSVファイル
+// ファイル選択時の処理
+const handleFileChange = (event) => {
+    selectedFile.value = event.target.files[0];
+};
 
-// CSVドラックアンドドロップ
-const handleFileUpload = (e) => {
-    file.value = e.target.files[0]
+// 店舗新規登録(CSVアップロード)
+const storeShops = async () => {
+    // バリデーション
+    if (validateCsv()) {
+        try {
+            const formData = new FormData();
+            formData.append('csv', selectedFile.value);
+
+            // トークンを取得
+            let token = localStorage.getItem('admin_auth_token');
+
+            const response = await axios.post(import.meta.env.VITE_API_URL + '/shops/store/csv', formData, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            // POSTが成功した場合の処理を追加
+            if (response.status === 201) {
+                // 完了画面に遷移
+                router.push({
+                    name: 'AdminComplete',
+                    state: { message: '店舗の登録が完了しました。' }
+                })
+            }
+        } catch (error) {
+            console.error("POST request:", error);
+            errorMessage.value = '店舗の登録に失敗しました: ' + error.response.data.message;
+        }
+    }
+}
+
+// CSVファイルのバリデーション
+const validateCsv = () => {
+    const file = selectedFile.value;
+    const maxSize = 1024 * 1024 * 2; // 2MBまでのみ許可
+    const validTypes = ['text/csv']; // CSVのみ許可
+
+    if (file) {
+        if (file.size > maxSize) {
+            errorMessage.value = 'ファイルサイズが大きすぎます。2MB以下のファイルを選択してください。';
+            return false;
+        } else if (!validTypes.includes(file.type)) {
+            errorMessage.value = '無効なファイルタイプです。CSVファイルのみアップロードできます。';
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        errorMessage.value = 'CSVファイルを選択してください。';
+        return false;
+    }
 }
 
 // 口コミ画面に遷移する
